eccc087633f18ec3b154f95d89410bfd5c2a12d3
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2024-07-23 16:20:53 -0500

    Create Saludo.java

---
+++ Fundamentals/julio_22/Saludo.java
@@ -0,0 +1,29 @@
+
+package Fundamentals.julio_22;
+
+class Saludo {
+
+    public static void main(String [] args){
+      
+        System.out.println("Hola Mundo"); // Imprime Hola Mundo en la consola
+
+        /*TIPOS DE VARIABLES*/
+        /* PRIMITIVOS */
+        int numero = 10; // Variable de tipo entero
+        double decimal = 10.5; // Variable de tipo decimal
+        char caracter = 'a'; // Variable de tipo caracter
+        boolean bandera = true; // Variable de tipo booleano
+        
+        /*NO PRIMITIVOS */
+        Integer numero2 = 10; // Variable de tipo entero
+        Double decimal2 = 10.5; // Variable de tipo decimal
+        Character caracter2 = 'a'; // Variable de tipo caracter
+        String cadena = "Hola Mundo"; // Variable de tipo cadena
+        Boolean bandera2 = true; // Variable de tipo booleano
+
+
+        
+
+
+    }
+}
