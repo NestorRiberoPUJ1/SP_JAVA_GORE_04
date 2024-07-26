51d9e357cd861d68a4a576d6269245d30bcd3b82
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2024-07-25 19:23:38 -0500

    update

--- POO/julio_25/MedioTransporte.java
+++ POO/julio_25/MedioTransporte.java
@@ -4,33 +4,120 @@ public class MedioTransporte {
 
     /* ATRIBUTOS (VARIABLES) */
     /* SON TODAS LA VARIABLES QUE DESCRIBEN MI CLASE */
-    public boolean esMasivo;
-    public String medio;
-    public boolean tieneAsientos;
-    public int pasajeros;
-    public String combustible;
-    public String tipo;
+    private boolean transporteMasivo;
+    private String medio;
+    private boolean asientos;
+    private int cantidadAsientos;
+    private int pasajeros;
+    private String combustible;
+    private String tipo;
 
     /* CONSTRUCTORES */
     public MedioTransporte() {
     }
-    public MedioTransporte(boolean esMasivo, String medio, boolean tieneAsientos, int pasajeros, String combustible,
-            String tipo) {
-        this.esMasivo = esMasivo;
+
+    public MedioTransporte(boolean transporteMasivo, String medio, boolean asientos, int cantidadAsientos,
+            int pasajeros, String combustible, String tipo) {
+        this.transporteMasivo = transporteMasivo;
         this.medio = medio;
-        this.tieneAsientos = tieneAsientos;
+        this.asientos = asientos;
+        this.cantidadAsientos = cantidadAsientos;
         this.pasajeros = pasajeros;
         this.combustible = combustible;
         this.tipo = tipo;
     }
 
-
-
-
-    
-    
-
     /* MÉTODOS */
     /* SON TODOS LOS COMPORTAMIENTOS QUE PUEDE REALIZAR MI CLASE */
+
+    public void encender() {
+        System.out.println("Encendiendo el " + tipo);
+    }
+
+    public void apagar() {
+        System.out.println("Apagando el " + tipo);
+    }
+
+    public void acelerar() {
+        System.out.println("Acelerando el " + tipo);
+    }
+
+    public void frenar() {
+        System.out.println("Frenando el " + tipo);
+    }
+
+    public void recogerPasajeros() {
+        System.out.println("Recogiendo pasajeros en el " + tipo);
+        System.out.println("Pasajeros a bordo: " + pasajeros);
+    }
+
+    public void recogerPasajeros(int nuevosPasajeros) {
+        this.pasajeros += nuevosPasajeros;
+        System.out.println("Recogiendo pasajeros en el " + tipo);
+        System.out.println("Pasajeros a bordo: " + pasajeros);
+    }
+
+    public int pasajerosSinAsiento() {
+        return pasajeros - cantidadAsientos;
+    }
+
+    /* GETTERS Y SETTERS */
+
+    public boolean isTransporteMasivo() {
+        return transporteMasivo;
+    }
+
+    public void setTransporteMasivo(boolean esMasivo) {
+        this.transporteMasivo = esMasivo;
+    }
+
+    public String getMedio() {
+        return medio;
+    }
+
+    public void setMedio(String medio) {
+        this.medio = medio;
+    }
+
+    public boolean isAsientos() {
+        return asientos;
+    }
+
+    public void setAsientos(boolean tieneAsientos) {
+        this.asientos = tieneAsientos;
+    }
+
+    public int getCantidadAsientos() {
+        return cantidadAsientos;
+    }
+
+    public void setCantidadAsientos(int asientos) {
+        this.cantidadAsientos = asientos;
+    }
+
+    public int getPasajeros() {
+        return pasajeros;
+    }
+
+    public void setPasajeros(int pasajeros) {
+        this.pasajeros = pasajeros;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
 
 }
--- POO/julio_25/MedioTransporteTest.java
+++ POO/julio_25/MedioTransporteTest.java
@@ -6,18 +6,28 @@ public class MedioTransporteTest {
         /* CREAR UNA INSTANCIA */
         // nombre de la clase + nombre del objeto = new + constructor
         MedioTransporte avion = new MedioTransporte();
-        avion.esMasivo = true;
-        avion.medio = "Aéreo";
-        avion.tieneAsientos = true;
-        avion.pasajeros = 200;
-        avion.combustible = "JET FUEL";
-        avion.tipo = "Avión";
+        avion.setTransporteMasivo(true);
+        avion.setMedio("Aereo");
+        avion.setAsientos(true);
+        avion.setCantidadAsientos(180);
+        avion.setPasajeros(200);
+        avion.setCombustible("JET FUEL");
+        avion.setTipo("Avion");
 
-        System.out.println("El medio de transporte es: " + avion.medio);
+        System.out.println("El medio de transporte es: " + avion.getMedio());
 
-        MedioTransporte carro = new MedioTransporte(false, "Terrestre", true, 5, "Gasolina", "Carro");
-        System.out.println("El medio de transporte es: " + carro.medio);
+        MedioTransporte carro = new MedioTransporte(false, "Terrestre", true, 5, 5, "Gasolina", "Carro");
+        System.out.println("El medio de transporte es: " + carro.getMedio());
 
-        
+        avion.encender();
+        System.out.println("Pasajeros sin asiento: " + avion.pasajerosSinAsiento());
+
+        carro.acelerar();
+
+
+        avion.recogerPasajeros();
+        avion.recogerPasajeros(10);
+        System.out.println("Pasajeros sin asiento: " + avion.pasajerosSinAsiento());
+
     }
 }
